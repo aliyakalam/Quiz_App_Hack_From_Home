133bd2627cd3083856d0649bfcd7f58c9d9e6703
Aliya A <72246025+aliyakalam@users.noreply.github.com>
2021-08-20 00:37:08 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which event occurs when the user clicks on an HTML element?",
+    answer : "onclick",
+    options: [
+      "onmouseclick ", 
+      "onclick",
+      "Onchange",
+      "onmouseover"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which operator is used to assign a value to a variable?",
+    answer: "=",
+    options: [
+      "*",
+      ":",
+      "-",
+      "="
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which one of these is a Javascript package manager?",
+    answer: "npm",
+    options: [
+      "  npm",
+      "node.js",
+      "npm.js",
+      "TypeScript"
+    ]
+  },
+    {
+    numb: 4,
+    question: "10.what is the output of 33==33.0 ?",
+    answer: "True",
+    options: [
+      "False",
+      "True",
+      "33.0",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is a server-side JavaScript object?",
+    answer: "File",
+    options: [
+      "Date",
+      "Function",
+      "FileUpload",
+      "File"
+    ]
+  },
+];
